5e7b9dc44ace098bfcb28a189859c5340bb5ec15
AgostinaArevaloJaube <agosarevalo@gmail.com>
2023-03-24 17:45:02 -0300

    Add prettier

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,21 +1,21 @@
-import type { StorybookConfig } from '@storybook/nextjs';
+import type { StorybookConfig } from "@storybook/nextjs";
 const config: StorybookConfig = {
-	stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
-	addons: [
-		'@storybook/addon-links',
-		'@storybook/addon-essentials',
-		'@storybook/addon-interactions',
-		'@storybook/addon-docs',
-		'@storybook/addon-viewport',
-		'@storybook/addon-a11y',
-		'@storybook/addon-mdx-gfm',
-	],
-	framework: {
-		name: '@storybook/nextjs',
-		options: {},
-	},
-	docs: {
-		autodocs: 'tag',
-	},
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
+    "@storybook/addon-docs",
+    "@storybook/addon-viewport",
+    "@storybook/addon-a11y",
+    "@storybook/addon-mdx-gfm",
+  ],
+  framework: {
+    name: "@storybook/nextjs",
+    options: {},
+  },
+  docs: {
+    autodocs: "tag",
+  },
 };
 export default config;
